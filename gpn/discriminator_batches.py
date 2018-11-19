1a66749ee2ad46b5310e34371dd097c15cd1e32e
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2018-11-19 23:39:23 +0300

    feat: Sample square fragments

--- gpn/discriminator_batches.py
+++ gpn/discriminator_batches.py
@@ -29,3 +29,31 @@ def pad_image(image_arr: np.ndarray, padding_size: int) -> np.ndarray:
     arrays = (horizontal_padding, image_arr, horizontal_padding)
     image_arr = np.concatenate(arrays, axis=2)
     return image_arr
+
+
+def sample_square_part(
+        image_arr: np.ndarray, internal_size: int, padding_size: int
+) -> np.ndarray:
+    """
+    Sample a square part of an image.
+
+    :param image_arr:
+        array of shape (channel_dim, x_dim, y_dim)
+    :param internal_size:
+        size of square that must be entirely within the original image
+    :param padding_size:
+        size of a frame that can be outside of the original image
+    :return:
+        square image of size `frame_size + padding_size`
+    """
+    x_options = list(range(image_arr.shape[1] - internal_size + 1))
+    x_corner = np.random.choice(x_options)
+    y_options = list(range(image_arr.shape[2] - internal_size + 1))
+    y_corner = np.random.choice(y_options)
+    padded_image_arr = pad_image(image_arr, padding_size)
+    x_min = x_corner + padding_size - 1
+    x_max = x_corner + internal_size + 2 * padding_size
+    y_min = y_corner + padding_size - 1
+    y_max = y_corner + internal_size + 2 * padding_size
+    part = padded_image_arr[:, x_min:x_max, y_min:y_max]
+    return part
--- tests/test_discriminator_batches.py
+++ tests/test_discriminator_batches.py
@@ -5,6 +5,8 @@ Author: Nikolay Lysenko
 """
 
 
+from typing import List
+
 import pytest
 import numpy as np
 
@@ -41,3 +43,62 @@ def test_pad_image(
     """Test `pad_image` function."""
     padded_image_arr = batches.pad_image(image_arr, padding_size)
     np.testing.assert_equal(padded_image_arr, expected)
+
+
+@pytest.mark.parametrize(
+    "image_arr, internal_size, padding_size, possible_outputs",
+    [
+        (
+            np.array([
+                [[0, 1],
+                 [1, 0]],
+                [[1, 1],
+                 [0, 0]],
+            ]),
+            1,
+            1,
+            [
+                np.array([
+                    [[0, 0, 0],
+                     [0, 0, 1],
+                     [0, 1, 0]],
+                    [[0, 0, 0],
+                     [0, 1, 1],
+                     [0, 0, 0]]
+                ]),
+                np.array([
+                    [[0, 0, 0],
+                     [0, 1, 0],
+                     [1, 0, 0]],
+                    [[0, 0, 0],
+                     [1, 1, 0],
+                     [0, 0, 0]]
+                ]),
+                np.array([
+                    [[0, 0, 1],
+                     [0, 1, 0],
+                     [0, 0, 0]],
+                    [[0, 1, 1],
+                     [0, 0, 0],
+                     [0, 0, 0]]
+                ]),
+                np.array([
+                    [[0, 1, 0],
+                     [1, 0, 0],
+                     [0, 0, 0]],
+                    [[1, 1, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]]
+                ])
+            ]
+        )
+    ]
+)
+def test_sample_square_part(
+        image_arr: np.ndarray, internal_size: int, padding_size: int,
+        possible_outputs: List[np.ndarray]
+) -> None:
+    """Test `sample_square_part` function."""
+    output = batches.sample_square_part(image_arr, internal_size, padding_size)
+    print(output)
+    assert any([np.array_equal(output, arr) for arr in possible_outputs])
